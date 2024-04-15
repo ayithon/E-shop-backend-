80926ed074b48cb9cd12cc3f5de5502871a032c4
Ayiton <166718890+ayithon@users.noreply.github.com>
2024-04-15 06:54:48 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['https://eshop-tutorial-pyri.vercel.app',],
+  origin: ['https://ecommercereva.netlify.app',],
   credentials: true
 }));
 
